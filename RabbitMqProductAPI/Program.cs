67b75f3ae49bae79a0ffa4beb3916b53664101d6
Greugreu <fenris.fitz@hotmail.fr>
2023-02-23 16:12:08 +0100

    COMMIT

--- RabbitMqProductAPI/Program.cs
+++ RabbitMqProductAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using RabbitMQ.Client;
 using RabbitMqProductAPI.Data;
 using RabbitMqProductAPI.RabbitMQ;
 using RabbitMqProductAPI.Services;
@@ -7,8 +9,27 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddDbContext<DbContextClass>();
+
 builder.Services.AddScoped<IRabbitMQProducer, RabbitMQProducer>();
 
+builder.Services.AddSingleton<IModel>(serviceProvider =>
+{
+    //Here we specify the Rabbit MQ Server. we use rabbitmq docker image and use it
+    var factory = new ConnectionFactory
+    {
+        HostName = builder.Configuration["RabbitMQ:Hostname"],
+        UserName = builder.Configuration["RabbitMQ:Username"],
+        Password = builder.Configuration["RabbitMQ:Password"]
+    };
+
+    //Create the RabbitMQ connection using connection factory details as i mentioned above
+    var connection = factory.CreateConnection();
+
+    //Here we create channel with session and model
+    var channel = connection.CreateModel();
+
+    return channel;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,6 +38,16 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Migrate latest database changes during startup
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider
+        .GetRequiredService<DbContextClass>();
+
+    // Here is the migration executed
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -24,7 +55,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
